3e52d3ce10e80ca9678846ac703ff72328779ea4
Fernando Batista <nandosbx@gmail.com>
2020-09-07 22:33:58 -0300

    Setting config key

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -1,5 +1,11 @@
-export const firebaseConfig = {
-  /**
-   * Your Firebase SDK snippet Config here.   */ 
+const { firebaseApp } = require('./key')
 
-};
+export const firebaseConfig = firebaseApp
+
+/*
+{
+    Create a key.js with your apikey or insert
+    your Firebase SDK snippet Config here.   
+    
+}
+*/
